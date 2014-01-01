b562222b0301b7cf12c79ea2076dac3eded46ab1
macheteshuffle <jeff.li029@gmail.com>
2014-01-01 18:54:18 -0500

    created index page with passing acceptance testing

--- db/migrate/20140101223546_create_cars.rb
+++ db/migrate/20140101223546_create_cars.rb
@@ -2,8 +2,9 @@ class CreateCars < ActiveRecord::Migration
   def change
     create_table :cars do |t|
       t.string :color, null:false
-      t.date :year, null:false
+      t.integer :year, null:false
       t.integer :mileage, null:false
+      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140101224026) do
+ActiveRecord::Schema.define(version: 20140101223546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cars", force: true do |t|
     t.string   "color",       null: false
-    t.date     "year",        null: false
+    t.integer  "year",        null: false
     t.integer  "mileage",     null: false
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text     "description"
   end
 
 end
